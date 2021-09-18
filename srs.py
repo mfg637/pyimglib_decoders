9e37210a646b73dcc8f57f497e02d3676d6ac584
mfg637 <mfg636@gmail.com>
2021-09-18 19:31:52 +0300

    MfGMediaServer: generate thumbnails for SRS files

--- srs.py
+++ srs.py
@@ -1,6 +1,5 @@
 from . import ACLMMP, ffmpeg_frames_stream
-from .video import open_video
-import json
+import pathlib
 
 SRS_FILE_HEADER = "{\"ftype\":\"CLSRS\""
 
@@ -10,10 +9,11 @@ def is_ACLMMP_SRS(file_path):
     file.close()
     return header == SRS_FILE_HEADER
 
-def decode(file_path):
+def decode(file_path: pathlib.Path):
+    dir = file_path.parent
     fp = open(file_path, "r")
     content_metadata, streams_metadata, minimal_content_compatibility_level = ACLMMP.srs_parser.parseJSON(fp)
     fp.close()
 
     video = streams_metadata[0].get_compatible_files(0)[0]
-    return ffmpeg_frames_stream.FFmpegFramesStream(video, original_filename=file_path)
+    return ffmpeg_frames_stream.FFmpegFramesStream(dir.joinpath(video), original_filename=file_path)
