f0278fe8909827b340337d4af9732f6904daf234
mfg637 <mfg636@gmail.com>
2021-09-18 20:19:22 +0300

    load cover and poster images from srs files

--- srs.py
+++ srs.py
@@ -1,5 +1,6 @@
 from . import ACLMMP, ffmpeg_frames_stream
 import pathlib
+import PIL.Image
 
 SRS_FILE_HEADER = "{\"ftype\":\"CLSRS\""
 
@@ -14,6 +15,12 @@ def decode(file_path: pathlib.Path):
     fp = open(file_path, "r")
     content_metadata, streams_metadata, minimal_content_compatibility_level = ACLMMP.srs_parser.parseJSON(fp)
     fp.close()
+    print(content_metadata)
 
-    video = streams_metadata[0].get_compatible_files(0)[0]
-    return ffmpeg_frames_stream.FFmpegFramesStream(dir.joinpath(video), original_filename=file_path)
+    if 'poster-image' in content_metadata:
+        return PIL.Image.open(dir.joinpath(content_metadata['poster-image']))
+    elif 'cover-image' in content_metadata:
+        return PIL.Image.open(dir.joinpath(content_metadata['cover-image']))
+    else:
+        video = streams_metadata[0].get_compatible_files(0)[0]
+        return ffmpeg_frames_stream.FFmpegFramesStream(dir.joinpath(video), original_filename=file_path)
